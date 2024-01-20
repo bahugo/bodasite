d52ff2e8abcec1608e74c8940394045e86c8a08f
bahugo <barsottihugo@gmail.com>
2024-01-20 19:48:20 +0100

    Corrected site and base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://bahugo.github.io/bodasite',
+  site: 'https://bahugo.github.io',
   base: '/bodasite',
 });
