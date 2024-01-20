22365b0efbd0ec5f232a6cbcb2ab09e6fb88644f
bahugo <barsottihugo@gmail.com>
2024-01-20 16:44:48 +0100

    Updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://astronaut.github.io',
+  site: 'https://github.com/bahugo/bodasite.github.io',
   // base: '/bodasite',
 });
