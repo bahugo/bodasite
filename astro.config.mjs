aee2bfa4f6d995ff25ae25bebc22c10fb77db487
Hugo BARSOTTI <barsottihugo@gmail.com>
2025-11-09 12:11:34 +0100

    Fixed roboto

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,4 @@ import { defineConfig, fontProviders } from 'astro/config';
 export default defineConfig({
   site: 'https://bahugo.github.io',
   base: '/bodasite',
-    experimental: {
-        fonts: [{
-            provider: fontProviders.google(),
-            name: "Roboto",
-            cssVariable: "--font-roboto"
-        }]
-    }
 });
