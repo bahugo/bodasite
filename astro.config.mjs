5c361bc467dd28ca530c0e9d00048bf77e46933c
Hugo BARSOTTI <barsottihugo@gmail.com>
2025-11-13 22:56:07 +0100

    Adding instagram link

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
 
 export default defineConfig({
   site: 'https://www.boda-architectes.com',
-  // base: '/bodasite',
+  integrations: [icon()],
 });
